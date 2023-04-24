a3d1ff7ae2237902cbbb8e27c3c7ef3d16b44cd5
Jimmy Clairy <jimcl@hotmail.fr>
2023-04-24 18:19:41 +0200

    Modif code

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-recevoirTemperature("paris")
-
 function recevoirTemperature(ville) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=d99e12053308e2b172415a9df53e0a5d&units=metric`;
 
     fetch(url)
-    .then(reponse => reponse.json())
-    .then(data => {
-        console.log(data);
-        if(data.cod === 200){
-            
-            let villeTemperature = data.name;
-            document.querySelector('.ville').textContent = villeTemperature;
-        
-            let temperature = data.main.temp;
-            document.querySelector('.temperature_label').textContent = Math.floor(temperature);
+        .then(reponse => reponse.json())
+        .then(data => {
+            console.log(data);
+            if (data.cod === 200) {
 
-            let temperatureMinimal = data.main.temp_min;
-            document.querySelector('.temp_min').textContent = Math.floor(temperatureMinimal);
+                document.querySelector('.ville').textContent = data.name;
 
-            let temperatureMaximal = data.main.temp_max;
-            document.querySelector('.temp_max').textContent = Math.floor(temperatureMaximal);
-            
-        } else {
-            alert("Une erreur est survenue ville non trouvée.");
-        }
-        
-    }) 
+                document.querySelector('.temperature_label').textContent = Math.floor(data.main.temp);
+
+                document.querySelector('.temp_min').textContent = Math.floor(data.main.temp_min);
+
+                document.querySelector('.temp_max').textContent = Math.floor(data.main.temp_max);
+
+            } else {
+                alert("Une erreur est survenue ville non trouvée.");
+            }
+        })
 }
 
 let changerDeVille = document.querySelector('.changer');
 
 changerDeVille.addEventListener('click', () => {
-    villeChoisie =  prompt('Entrez une ville');
+    villeChoisie = prompt('Entrez une ville');
     recevoirTemperature(villeChoisie);
-    }
+}
 )
+
+recevoirTemperature("paris")
